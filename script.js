86681bd9f0b836d90dd9b1ecde4d32a8e193a4dc
mkiran <152247979+M6839@users.noreply.github.com>
2024-11-23 18:42:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+var headerImg=document.getElementsById("headerImg");
+var img=document.getElementById("img")
+var h1=document.getElementById("h1");
+var h2=document.getElementById("h2");
+
+h1.onclick=function(){
+ 
+    img.style.left=50+"px";
+}
